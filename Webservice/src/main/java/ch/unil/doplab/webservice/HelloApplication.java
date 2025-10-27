9eed9b54f2a618031d65c2f8a9b9a7725764b1cb
NikhileshAch <nikhilesh.acharya@unil.ch>
2025-10-27 09:44:25 +0100

    init

---
+++ Webservice/src/main/java/ch/unil/doplab/webservice/HelloApplication.java
@@ -0,0 +1,9 @@
+package ch.unil.doplab.webservice;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
